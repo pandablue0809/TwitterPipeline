de7a97ffee4955fa93a02204d61053b99e0fec0d
pandablue0809 <bluep0415@gmail.com>
2022-10-14 12:21:39 +0530

    Update twitter_commands.sh

--- twitter_commands.sh
+++ twitter_commands.sh
@@ -4,3 +4,15 @@ sudo pip install apache-airflow
 sudo pip install pandas 
 sudo pip install s3fs
 sudo pip install tweepy
+
+# Setting the Airflow Server
+cd airflow
+sudo nano airflow.cfg
+# Change the dags_folder as /twitter_dag
+
+mkdir twitter_dag
+cd twitter_dag
+
+# Copy the Codes on Server
+sudo nano twitter_etl.py
+sudo nano twitter_dag.py
